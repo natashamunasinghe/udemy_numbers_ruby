a6099fa57c4b2fbf37fb6857120e09e502c4858f
Your Name <you@example.com>
2018-10-04 08:44:51 +1000

    add numbers

---
+++ lecture20_numbers.rb
@@ -0,0 +1,20 @@
+#Working with numbers in Ruby
+puts "What's your first number?"
+first_number = gets.chomp.to_i
+
+puts "What's your second number?"
+second_number = gets.chomp.to_i
+
+#Multiplication
+puts "The result of multiplying your first and second numbers is #{first_number * second_number}"
+
+#Subtraction
+puts "The result of subtracting your first and second numbers is #{first_number - second_number}"
+
+#Division
+puts "The result of dividing your first from your second numbers is #{first_number / second_number}"
+
+#Modulo
+puts "The modulo of your first and second numbers is #{first_number % second_number}"
+
+
